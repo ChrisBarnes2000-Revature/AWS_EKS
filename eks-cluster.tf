c06d27fdbe25d827d9029c14e98bdd8cef91a0ff
Bryon Smith <webmastersmith37@gmail.com>
2022-08-01 21:40:39 -0500

    add helm

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,70 +1,143 @@
-# Cluster
+# eks module
+# https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "18.26.6"
 
+  # cluster specs
   cluster_name    = local.cluster_name
   cluster_version = "1.22"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
 
+  cluster_security_group_additional_rules = {
+    egress_nodes_ephemeral_ports_tcp = {
+      description                = "To node 1025-65535"
+      protocol                   = "tcp"
+      from_port                  = 1025
+      to_port                    = 65535
+      type                       = "egress"
+      source_node_security_group = true
+    }
+  }
+
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+    egress_all = {
+      description      = "Node all egress"
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      type             = "egress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  }
+
+  # cluster t3.large x 2
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-    # ami_id = "ami-052efd3df9dad4825"  # ubuntu server LTS 20.04
+    ami_type       = "AL2_x86_64"
+    instance_types = ["t2.micro"]
 
     attach_cluster_primary_security_group = true
-
-    # Disabling and using externally provided security groups
-    create_security_group = false
+    vpc_security_group_ids                = [aws_security_group.default.id]
   }
 
   eks_managed_node_groups = {
-    # aws_launch_template
-    # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_template
-    one = {
-      name = "worker-node-group-1"
+    default_node_group = {
+      # By default, the module creates a launch template to ensure tags are propagated to instances, etc.,
+      # so we need to disable it to use the default template provided by the AWS EKS managed node group service
+      min_size               = 1
+      max_size               = 2
+      desired_size           = 2
+      create_launch_template = false
+      launch_template_name   = ""
+      ami_id                 = "ami-052efd3df9dad4825"
+      # disk_size              = 50
+      instance_types = ["t2.micro"]
+      # capacity_type          = "SPOT"
+      # Remote access cannot be specified with a launch templatecd 
 
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      # pre_bootstrap_user_data = <<-EOT
-      # echo 'foo bar'
-      # EOT
-
-      vpc_security_group_ids = [
-        aws_security_group.node_group_one.id
-      ]
-
-      key_name = aws_key_pair.eks_nodes.key_name
+      remote_access = {
+        ec2_ssh_key = aws_key_pair.ssh_access_key.key_name
+      }
     }
-
-    # two = {
-    #   name = "worker-node-group-2"
-
-    #   instance_types = ["t3.medium"]
-
-    #   min_size     = 1
-    #   max_size     = 2
-    #   desired_size = 1
-
-    #   pre_bootstrap_user_data = <<-EOT
-    #   echo 'foo bar'
-    #   EOT
-
-    #   vpc_security_group_ids = [
-    #     aws_security_group.node_group_two.id
-    #   ]
-
-    #   key_name = aws_key_pair.eks_nodes.key_name
-    # }
   }
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
+  # COE provided
+  # aws_auth_users = [
+  #   {
+  #     userarn  = aws_iam_user.eks-user.arn
+  #     username = aws_iam_user.eks-user.name
+  #     groups   = ["system:masters"]
+  #   },
+  # ]
+
+  tags = {
+    Name      = "${var.user_name}-cluster"
+    Terraform = "true"
+  }
+} # end module eks
+
+# resource "aws_iam_user" "eks-user" {
+#   name = "2206-devops-user"
+
+#   tags = {
+#     tag-key = "2206-devops-user"
+#   }
+# }
+
+# resource "aws_iam_user_policy" "eks-iam-user_policy" {
+#   name = "2206-devops-eks-access-policy"
+#   user = aws_iam_user.eks-user.name
+
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Sid    = "2206-devops-user",
+#         Effect = "Allow",
+#         Action = [
+#           "eks:AccessKubernetesApi",
+#           "eks:DescribeCluster",
+#           "eks:CreateNodegroup",
+#           "eks:DeleteNodegroup",
+#           "eks:DescribeNodegroup",
+#           "eks:DescribeUpdate",
+#           "eks:ListNodegroups",
+#           "eks:ListTagsForResource",
+#           "eks:UpdateClusterConfig",
+#           "eks:UpdateNodegroupConfig"
+#         ],
+#         Resource = "${module.eks.cluster_arn}"
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_access_key" "eks-access-key" {
+#   user = aws_iam_user.eks-user.name
+# }
+
+resource "aws_key_pair" "ssh_access_key" {
+  key_name   = "2206-devops-key"
+  public_key = file(".ssh/id_rsa.pub")
 }
 
-resource "aws_key_pair" "eks_nodes" {
-  key_name   = "eks-node-key"
-  public_key = file("./.ssh/id_rsa.pub")
-}
+# output "aws-keys" {
+#   value = {
+#     access_key = aws_iam_access_key.eks-access-key.id
+#     secret_key = aws_iam_access_key.eks-access-key.secret
+#   }
+#   sensitive = true
+# }
--- main.tf
+++ main.tf
@@ -17,9 +17,16 @@ terraform {
 
 # The Kubernetes provider is included in this file so the EKS module can complete successfully. Otherwise, it throws an error when creating `kubernetes_config_map.aws_auth`.
 # You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = module.eks.cluster_certificate_authority_data
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+      command     = "aws"
+    }
+  }
 }
 
 provider "aws" {
